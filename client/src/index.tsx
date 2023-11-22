5679a1d3c6c37bb7cbf781ec1c838f08ca64bf6f
oooihmm <oooihmm@naver.com>
2023-11-23 02:25:13 +0900

    Feat: 모든 route에서 header가 상단에 존재하도록 추가

---
+++ client/src/index.tsx
@@ -0,0 +1,16 @@
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
+import Header from "./components/Header/Header/Header";
+import App from "./App";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
+root.render(
+  <BrowserRouter>
+    <Header />
+    <App />
+  </BrowserRouter>
+);
