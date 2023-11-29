4ff9bf30d4c161ee2205170e58beac025046b131
oooihmm <oooihmm@naver.com>
2023-11-30 01:56:03 +0900

    Feat: 홈페이지 작성

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,78 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios, { AxiosResponse } from "axios";
+import Dashboard from "../style/dashboard";
+import Contents from "../style/contents";
+import SearchContainer from "../components/SearchContainer";
+import ImageContainer from "../components/ImageContainer";
+import ImageWrapper from "../style/imagewrapper";
+import { imageData, Urls } from "../interface/unsplash";
 
-const Home = () => {
-  return <div>Home</div>;
+type HomeProps = {
+  bookmark: imageData[];
+  handleBookmark: (image: imageData) => void;
+};
+
+const Home = ({ bookmark, handleBookmark }: HomeProps) => {
+  const [imageList, setImageList] = useState<imageData[]>([]);
+  const [BackgroundImage, setBackgroundImage] = useState<Urls | {}>({});
+
+  useEffect(() => {
+    axios
+      .get("https://api.unsplash.com/photos/random", {
+        params: {
+          client_id: process.env.REACT_APP_ACCESS_KEY,
+          count: 12,
+        },
+      })
+      .then((response: AxiosResponse<imageData[]>) => {
+        const rawImageData: imageData[] = response.data;
+        setImageList(rawImageData);
+        console.log(rawImageData);
+      })
+      .catch((error: string) => {
+        console.error(error);
+      });
+  }, []);
+
+  const chunkSize = Math.round(imageList.length / 3);
+
+  return (
+    <Dashboard>
+      <SearchContainer></SearchContainer>
+      <Contents>
+        <ImageWrapper>
+          {imageList.slice(0, chunkSize).map((image) => (
+            <ImageContainer
+              key={image.id}
+              image={image}
+              bookmark={bookmark}
+              handleBookmark={handleBookmark}
+            />
+          ))}
+        </ImageWrapper>
+        <ImageWrapper>
+          {imageList.slice(chunkSize, chunkSize * 2).map((image) => (
+            <ImageContainer
+              key={image.id}
+              image={image}
+              bookmark={bookmark}
+              handleBookmark={handleBookmark}
+            />
+          ))}
+        </ImageWrapper>
+        <ImageWrapper>
+          {imageList.slice(chunkSize * 2, imageList.length).map((image) => (
+            <ImageContainer
+              key={image.id}
+              image={image}
+              bookmark={bookmark}
+              handleBookmark={handleBookmark}
+            />
+          ))}
+        </ImageWrapper>
+      </Contents>
+    </Dashboard>
+  );
 };
 
 export default Home;
