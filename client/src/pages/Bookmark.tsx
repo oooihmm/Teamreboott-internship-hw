99ccb9219193c191bd5023fdf69503da7effacf3
oooihmm <oooihmm@naver.com>
2023-11-30 01:56:13 +0900

    Feat: 북마크 페이지 작성

--- client/src/pages/Bookmark.tsx
+++ client/src/pages/Bookmark.tsx
@@ -1,7 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios, { AxiosResponse } from "axios";
+import Dashboard from "../style/dashboard";
+import Contents from "../style/contents";
+import ImageContainer from "../components/ImageContainer";
+import ImageWrapper from "../style/imagewrapper";
+import { imageData, Urls } from "../interface/unsplash";
 
-const Bookmark = () => {
-  return <div>Bookmark</div>;
+type BookmarkProps = {
+  bookmark: imageData[];
+  handleBookmark: (image: imageData) => void;
+};
+
+const Bookmark = ({ bookmark, handleBookmark }: BookmarkProps) => {
+  const chunkSize = Math.round(bookmark.length / 3);
+
+  return (
+    <Dashboard>
+      <Contents>
+        <ImageWrapper>
+          {bookmark.slice(0, chunkSize).map((image) => (
+            <ImageContainer
+              key={image.id}
+              image={image}
+              bookmark={bookmark}
+              handleBookmark={handleBookmark}
+            />
+          ))}
+        </ImageWrapper>
+        <ImageWrapper>
+          {bookmark.slice(chunkSize, chunkSize * 2).map((image) => (
+            <ImageContainer
+              key={image.id}
+              image={image}
+              bookmark={bookmark}
+              handleBookmark={handleBookmark}
+            />
+          ))}
+        </ImageWrapper>
+        <ImageWrapper>
+          {bookmark.slice(chunkSize * 2, bookmark.length).map((image) => (
+            <ImageContainer
+              key={image.id}
+              image={image}
+              bookmark={bookmark}
+              handleBookmark={handleBookmark}
+            />
+          ))}
+        </ImageWrapper>
+      </Contents>
+    </Dashboard>
+  );
 };
 
 export default Bookmark;
