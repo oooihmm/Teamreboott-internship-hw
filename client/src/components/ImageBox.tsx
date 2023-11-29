bcfda277979b9871e7ce9ce3bcfb97326c1008bc
oooihmm <oooihmm@naver.com>
2023-11-30 06:20:44 +0900

    Feat: 이미지 상세 모달 생성

--- client/src/components/ImageBox.tsx
+++ client/src/components/ImageBox.tsx
@@ -3,21 +3,22 @@ import styled from "styled-components";
 import { useState } from "react";
 import { ImageData } from "../interface/unsplash";
 
-import Bookmark from "../style/bookmark";
+import ImageDetail from "./ImageDetail";
+import BookmarkButton from "./BookmarkButton";
 
-const Image = styled.div`
-  img {
-    width: 100%;
-    min-width: 350px;
-
-    min-height: 300px;
-    max-height: 400px;
-    object-fit: cover;
-  }
-
+const ImageBoxStyle = styled.div`
   padding: 0px;
   margin: 0 10px 20px 10px;
   position: relative;
+`;
+
+const Image = styled.img`
+  width: 100%;
+  min-width: 350px;
+
+  min-height: 300px;
+  max-height: 400px;
+  object-fit: cover;
 `;
 
 type ImageProps = {
@@ -31,33 +32,46 @@ const ImageBox = ({ image, bookmark, handleBookmark }: ImageProps) => {
     (bookmarkedImage) => bookmarkedImage.id === image.id
   );
 
+  const link = image.urls.small;
+  const description = image.alt_description;
+
   const [bookmarkColors, setBookmarkColors] = useState({
     background: isBookmarked ? "#f15151" : "white",
+    border: isBookmarked ? "#0000" : "#d1d1d1",
     color: isBookmarked ? "white" : "black",
   });
 
-  const link = image.urls.small;
-  const description = image.alt_description;
+  const [isModalOpen, setModalOpen] = useState(false);
 
   const handleClick = () => {
     setBookmarkColors({
       background: "#f15151",
+      border: "#0000",
       color: "white",
     });
     handleBookmark(image);
   };
 
+  const handleModalOpen = () => {
+    setModalOpen(true);
+  };
+
+  const handleModalClose = () => {
+    setModalOpen(false);
+  };
+
   return (
-    <Image>
-      <Bookmark
-        $background={bookmarkColors.background}
-        $color={bookmarkColors.color}
-        onClick={handleClick}
-      >
-        ♥︎
-      </Bookmark>
-      <img src={link} alt={description} />
-    </Image>
+    <ImageBoxStyle>
+      <BookmarkButton bookmarkColors={bookmarkColors} onClick={handleClick} />
+      <Image src={link} alt={description} onClick={handleModalOpen} />
+      <ImageDetail
+        image={image}
+        bookmark={bookmark}
+        handleBookmark={handleBookmark}
+        isModalOpen={isModalOpen}
+        handleModalClose={handleModalClose}
+      />
+    </ImageBoxStyle>
   );
 };
 
