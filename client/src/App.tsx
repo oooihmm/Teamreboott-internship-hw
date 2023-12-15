670aee7d05e2d1eb3d5d31cf1f8f6e3b24980923
jinseon choi <sttaurt@gmail.com>
2023-12-15 14:03:20 +0900

    Refactor: 헤더 import 위치 변경

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,65 +1,77 @@
 import React, { useState, useEffect } from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
 import GlobalStyle from "./style/global";
 
+import Header from "./components/Header";
 import Home from "./pages/Home";
 import Search from "./pages/Search";
 import Bookmark from "./pages/Bookmark";
 import { ImageData } from "./interface/unsplash";
 
 function App() {
-  const [bookmark, setBookmark] = useState<ImageData[]>([]);
+	const [bookmark, setBookmark] = useState<ImageData[]>([]);
 
-  useEffect(() => {
-    const savedBookmark = localStorage.getItem("savedBookmark");
-    if (savedBookmark) {
-      setBookmark(JSON.parse(savedBookmark));
-    }
-  }, []);
+	useEffect(() => {
+		const savedBookmark = localStorage.getItem("savedBookmark");
+		if (savedBookmark) {
+			setBookmark(JSON.parse(savedBookmark));
+		}
+	}, []);
 
-  useEffect(() => {
-    localStorage.setItem("savedBookmark", JSON.stringify(bookmark));
-  }, [bookmark]);
+	useEffect(() => {
+		localStorage.setItem("savedBookmark", JSON.stringify(bookmark));
+	}, [bookmark]);
 
-  const handleBookmark = (image: ImageData) => {
-    const isBookmarked = bookmark.some(
-      (bookmarkedImage) => bookmarkedImage.id === image.id
-    );
+	const handleBookmark = (image: ImageData) => {
+		const isBookmarked = bookmark.some(
+			(bookmarkedImage) => bookmarkedImage.id === image.id
+		);
 
-    if (isBookmarked) {
-      setBookmark(
-        bookmark.filter((bookmarkedImage) => bookmarkedImage.id !== image.id)
-      );
-    } else {
-      setBookmark([...bookmark, image]);
-    }
-  };
+		if (isBookmarked) {
+			setBookmark(
+				bookmark.filter((bookmarkedImage) => bookmarkedImage.id !== image.id)
+			);
+		} else {
+			setBookmark([...bookmark, image]);
+		}
+	};
 
-  return (
-    <>
-      <GlobalStyle />
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <Home handleBookmark={handleBookmark} bookmark={bookmark} />
-          }
-        ></Route>
-        <Route
-          path="search"
-          element={
-            <Search handleBookmark={handleBookmark} bookmark={bookmark} />
-          }
-        ></Route>
-        <Route
-          path="bookmark"
-          element={
-            <Bookmark handleBookmark={handleBookmark} bookmark={bookmark} />
-          }
-        ></Route>
-      </Routes>
-    </>
-  );
+	const Layout = () => {
+		return (
+			<div>
+				<Header />
+				<Outlet />
+			</div>
+		);
+	};
+
+	return (
+		<>
+			<GlobalStyle />
+			<Routes>
+				<Route path="/" element={<Layout />}>
+					<Route
+						index
+						element={
+							<Home handleBookmark={handleBookmark} bookmark={bookmark} />
+						}
+					></Route>
+					<Route
+						path="search"
+						element={
+							<Search handleBookmark={handleBookmark} bookmark={bookmark} />
+						}
+					></Route>
+					<Route
+						path="bookmark"
+						element={
+							<Bookmark handleBookmark={handleBookmark} bookmark={bookmark} />
+						}
+					></Route>
+				</Route>
+			</Routes>
+		</>
+	);
 }
 
 export default App;
