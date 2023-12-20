569cde47a93811229b497c4b0a4127b5e177b10c
jinseon choi <sttaurt@gmail.com>
2023-12-21 00:25:54 +0900

    Feat: search 페이지 reactQuery 도입

--- client/src/pages/Search.tsx
+++ client/src/pages/Search.tsx
@@ -1,61 +1,57 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import styled from "styled-components";
 import { useLocation } from "react-router-dom";
-import axios, { AxiosResponse } from "axios";
 import Dashboard from "../style/dashboard";
 import Contents from "../style/contents";
 import SearchBox from "../components/SearchBox";
 import ImageContainer from "../components/ImageContainer";
-import { SearchData, ImageData } from "../interface/unsplash";
+import { ImageData } from "../interface/unsplash";
+import { useQuery } from "react-query";
+import { fetchSearchPhotos } from "../api";
 
 type SearchProps = {
-  bookmark: ImageData[];
-  handleBookmark: (image: ImageData) => void;
+	bookmark: ImageData[];
+	handleBookmark: (image: ImageData) => void;
 };
 
 const SearchTitle = styled.span`
-  font-size: 45px;
-  font-weight: 900;
+	font-size: 45px;
+	font-weight: 900;
 `;
 
 const Search = ({ bookmark, handleBookmark }: SearchProps) => {
-  const [imageList, setImageList] = useState<ImageData[]>([]);
+	const location = useLocation();
+	const query = new URLSearchParams(location.search).get("query");
 
-  const location = useLocation();
-  const query = new URLSearchParams(location.search).get("query");
+	const { data, isLoading, isError } = useQuery(["searchPhotos", query], () =>
+		fetchSearchPhotos(query)
+	);
 
-  useEffect(() => {
-    if (query) {
-      axios
-        .get(`https://api.unsplash.com/search/photos?query=${query}`, {
-          params: {
-            client_id: process.env.REACT_APP_ACCESS_KEY,
-            per_page: 12,
-          },
-        })
-        .then((response: AxiosResponse<SearchData>) => {
-          const imageData: ImageData[] = response.data.results;
-          setImageList(imageData);
-        })
-        .catch((error: string) => {
-          console.error(error);
-        });
-    }
-  }, [query]);
+	const SearchImageContainer = () => {
+		if (isLoading) {
+			return <div>isLoding...</div>;
+		} else if (isError) {
+			return <div>error</div>;
+		} else {
+			return (
+				<ImageContainer
+					imageList={data["results"]}
+					bookmark={bookmark}
+					handleBookmark={handleBookmark}
+				/>
+			);
+		}
+	};
 
-  return (
-    <Dashboard>
-      <Contents>
-        <SearchTitle>{query}</SearchTitle>
-        <SearchBox />
-        <ImageContainer
-          imageList={imageList}
-          bookmark={bookmark}
-          handleBookmark={handleBookmark}
-        />
-      </Contents>
-    </Dashboard>
-  );
+	return (
+		<Dashboard>
+			<Contents>
+				<SearchTitle>{query}</SearchTitle>
+				<SearchBox />
+				<SearchImageContainer />
+			</Contents>
+		</Dashboard>
+	);
 };
 
 export default Search;
