f51e16b1e2b974b012cfb9fd584d430cc85ba8dc
jinseon choi <sttaurt@gmail.com>
2024-01-02 16:54:22 +0900

    Feat: 북마크 여부를 image container에서 계산하게 수정

--- client/src/components/ImageContainer.tsx
+++ client/src/components/ImageContainer.tsx
@@ -1,35 +1,54 @@
-import React from "react";
+import React, { useMemo } from "react";
 import ImageBox from "../components/ImageBox";
 import ImageWrapper from "../style/imagewrapper";
 import { ImageData } from "../interface/unsplash";
+import { useBookmarkStore } from "../store/useBookmarkStore";
 
 type imageContainerProps = {
-	imageList: ImageData[];
+  imageList: ImageData[];
 };
 
 const ImageContainer = ({ imageList }: imageContainerProps) => {
-	const chunkSize = Math.round(imageList.length / 3);
+  const chunkSize = Math.round(imageList.length / 3);
+  const { bookmarks } = useBookmarkStore();
 
-	return (
-		<>
-			{" "}
-			<ImageWrapper>
-				{imageList.slice(0, chunkSize).map((image) => (
-					<ImageBox key={image.id} image={image} />
-				))}
-			</ImageWrapper>
-			<ImageWrapper>
-				{imageList.slice(chunkSize, chunkSize * 2).map((image) => (
-					<ImageBox key={image.id} image={image} />
-				))}
-			</ImageWrapper>
-			<ImageWrapper>
-				{imageList.slice(chunkSize * 2, imageList.length).map((image) => (
-					<ImageBox key={image.id} image={image} />
-				))}
-			</ImageWrapper>
-		</>
-	);
+  const comparedImageList = useMemo(() => {
+    const compareBookmark = (image: ImageData) => {
+      return bookmarks.some(
+        (bookmarkedImage) => bookmarkedImage.id === image.id
+      );
+    };
+
+    const checkedImageList = imageList.map((image) => ({
+      ...image,
+      isBookmarked: compareBookmark(image),
+    }));
+
+    return checkedImageList;
+  }, [imageList, bookmarks]);
+
+  return (
+    <>
+      {" "}
+      <ImageWrapper>
+        {comparedImageList.slice(0, chunkSize).map((image) => (
+          <ImageBox key={image.id} image={image} />
+        ))}
+      </ImageWrapper>
+      <ImageWrapper>
+        {comparedImageList.slice(chunkSize, chunkSize * 2).map((image) => (
+          <ImageBox key={image.id} image={image} />
+        ))}
+      </ImageWrapper>
+      <ImageWrapper>
+        {comparedImageList
+          .slice(chunkSize * 2, imageList.length)
+          .map((image) => (
+            <ImageBox key={image.id} image={image} />
+          ))}
+      </ImageWrapper>
+    </>
+  );
 };
 
 export default ImageContainer;
