5b3cbd17cb6dd8e83fb5415fb6627bc28c7f09a0
jinseon choi <sttaurt@gmail.com>
2024-01-02 16:55:22 +0900

    Fix: 북마크 버튼 색상 관련 로직 삭제, props 변경

--- client/src/components/ImageBox.tsx
+++ client/src/components/ImageBox.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { useState } from "react";
-import { ImageData } from "../interface/unsplash";
+import { ComparedImageData } from "../interface/unsplash";
 
 import ImageModal from "./Modal/ImageModal";
 import BookmarkButton from "./BookmarkButton";
@@ -23,49 +23,19 @@ const Image = styled.img`
 `;
 
 type ImageProps = {
-	image: ImageData;
+	image: ComparedImageData;
 };
 
 const ImageBox = ({ image }: ImageProps) => {
-	const { bookmarks, addBookmark, removeBookmark } = useBookmarkStore();
-
-	const isBookmarked = bookmarks.some(
-		(bookmarkedImage) => bookmarkedImage.id === image.id
-	);
+	const { addBookmark, removeBookmark } = useBookmarkStore();
 
 	const link = image.urls.small;
 	const description = image.alt_description;
 
-	const [bookmarkColors, setBookmarkColors] = useState({
-		background: isBookmarked ? "#f15151" : "white",
-		border: isBookmarked ? "#0000" : "#d1d1d1",
-		color: isBookmarked ? "white" : "black",
-	});
-
 	const [isModalOpen, setModalOpen] = useState(false);
 
 	const handleClick = () => {
-		isBookmarked ? handleRemoveBookmark() : handleAddBookmark();
-	};
-
-	const handleAddBookmark = () => {
-		addBookmark(image);
-
-		setBookmarkColors({
-			background: "#f15151",
-			border: "#0000",
-			color: "white",
-		});
-	};
-
-	const handleRemoveBookmark = () => {
-		removeBookmark(image);
-
-		setBookmarkColors({
-			background: "white",
-			border: "#d1d1d1",
-			color: "black",
-		});
+		image.isBookmarked ? removeBookmark(image) : addBookmark(image);
 	};
 
 	const handleModalOpen = () => {
@@ -78,7 +48,7 @@ const ImageBox = ({ image }: ImageProps) => {
 
 	return (
 		<ImageBoxStyle>
-			<BookmarkButton bookmarkColors={bookmarkColors} onClick={handleClick} />
+			<BookmarkButton isBookmarked={image.isBookmarked} onClick={handleClick} />
 			<Image src={link} alt={description} onClick={handleModalOpen} />
 			<ImageModal
 				image={image}
